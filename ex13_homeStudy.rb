a43b5bee0f353aa4f879a8f35aac17db11b5bf86
erezsh <erezsh@liveperson.com>
2017-06-28 00:41:30 +0300

    adding ex13/4

---
+++ ex13_homeStudy.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+print "Enter VAR: "
+var = $stdin.gets.chomp
+puts "My second variable is: #{second}"
+puts "My first variable is: #{first}"
+puts "My third variable is: #{third}"
+
+puts var
