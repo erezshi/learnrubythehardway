46bfa01d883d342d0c79a2de340a1c4ea3fd4689
erezsh <erezsh@liveperson.com>
2017-07-10 18:53:12 +0300

    ex18/9/20

---
+++ ex18_hs.rb
@@ -0,0 +1,23 @@
+def checklist()
+checklistIndex = """
+\t1)Did you start your function definition with def?
+\t2)Does your function name have only characters and _ (underscore) characters?
+\t3)Did you put an open parenthesis ( right after the function name?
+\t4)Did you put your arguments after the parenthesis ( separated by commas?
+\t5)Did you make each argument unique (meaning no duplicated names)?
+\t6)Did you put a close parenthesis ) after the arguments?
+\t7)Did you indent all lines of code you want \in the function two spaces?
+\t8)Did you \end your function with \e\n\d lined up with the \def above?
+
+\t)When you run ("use" or "call") a function, check these things:
+
+\t1)Did you call/use/run this function by typing its name?
+\t2)Did you put the ( character after the name to run it?
+\t3)Did you put the values you want into the parenthesis separated by commas?
+\t4)Did you end the function call with a ) character?
+\t5)Functions that don't have parameters do not need the () after them, but would it be clearer if you wrote them anyway?
+Use these two checklists on the remaining lessons until you do not need them anymore.
+Finally, repeat this a few times to yourself:
+"""
+end
+checklist()
